4c551b2829fce5c9300142d44ef054420c1dd565
Intellisys <intellisys@Intellisyss-MacBook-Pro-3.local>
2025-08-27 16:04:37 -0400

    Just kiting with the styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="navBar">HOLAA</div>
         {children}
       </body>
     </html>
--- src/app/standups/page.tsx
+++ src/app/standups/page.tsx
@@ -1,3 +1,18 @@
+import { wrap } from "module";
+
 export default function Standup() {
-    return (<> Here we going to have the standups</>)
+    return (<> 
+    <div className="standups">
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <img src="https://picsum.photos/200/300" alt=""/>
+        <img src="https://picsum.photos/200/300" alt=""/>
+
+    </div>
+    </>)
 }
