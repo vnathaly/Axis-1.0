99f52e866f4d0457c90019d1c5235a69ed73b59a
Intellisys <intellisys@Intellisyss-MacBook-Pro-3.local>
2025-09-04 14:57:25 -0400

    Login and NavBar added

--- src/app/standups/page.tsx
+++ src/app/standups/page.tsx
@@ -4,7 +4,6 @@ export default function Standup() {
     return (
     <> 
         <div className="standups">
-            
             <img src="https://picsum.photos/200/300" alt=""/>
             <img src="https://picsum.photos/200/300" alt=""/>
             <img src="https://picsum.photos/200/300" alt=""/>
