61e686adaaf08ed1c5cf27492114273daba2a74a
Intellisys <intellisys@Intellisyss-MacBook-Pro-3.local>
2025-09-09 16:01:53 -0400

    Grid division done

--- src/app/reprolist/page.tsx
+++ src/app/reprolist/page.tsx
@@ -5,16 +5,34 @@ export default function reproList() {
         <>
          <div className="grid grid-cols-3 p-9"> 
             <div className="w-auto h-full col-span-2">
-                <div className="h-133 rounded-md bg-violet-400 m-3">
-
+                <div className="h-133 rounded-md  bg-gray-600 m-3 ">
+                    {/* Aqui va un video */}
                 </div>
-                <div className="h-50 rounded-md bg-violet-300 p-2 m-3">Descripcion</div>
-                <div className="grid grid-cols-1 gap-2 bg-violet-300 m-3 p-2">Comentarios</div>
+                <div className="h-50 rounded-md bg-gray-600 p-2 m-3">Descripcion</div>
+                <div className="grid grid-cols-1 gap-2  bg-gray-600 m-3 p-2">Comentarios</div>
             </div>
             <div className=" h-full grid grid-cols-1 gap-3 p-2">
-                <div className="mt-2 bg-violet-400 rounded-md">Este es un video</div>
-                <div className=" bg-violet-400 rounded-md">Este es un video</div>
-                <div className=" bg-violet-400 rounded-md">Este es un video</div>
+                <div className="mt-1 bg-gray-600 rounded-md">
+                    <figure>
+                        <video className="w-full" src="/videoprueba.mp4">
+                            {/* <source src="/videoprueba.mp4"/> */}
+                        </video>
+                    </figure>
+                </div>
+                <div className=" bg-gray-600 rounded-md">
+                    <figure>
+                        <video className="w-full" src="/videoprueba.mp4">
+                            {/* <source src="/videoprueba.mp4"/> */}
+                        </video>
+                    </figure>
+                </div>
+                <div className=" bg-gray-600 rounded-md">
+                    <figure>
+                        <video className="w-full" src="/videoprueba.mp4">
+                            {/* <source src="/videoprueba.mp4"/> */}
+                        </video>
+                    </figure>
+                </div>
             </div>
          </div>
         </>
