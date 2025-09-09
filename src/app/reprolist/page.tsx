2ed1de7ddf4878ec78383ceb2f033da23af504f5
Intellisys <intellisys@Intellisyss-MacBook-Pro-3.local>
2025-09-09 15:05:49 -0400

    Visual division

--- src/app/reprolist/page.tsx
+++ src/app/reprolist/page.tsx
@@ -5,12 +5,13 @@ export default function reproList() {
         <>
          <div className="grid grid-cols-3 gap-2 p-9"> 
             <div className="w-auto h-full gap-2 col-span-2">
-                <div className=" h-133">Video</div>
-                <div className=" h-50 rounded-md ">Descripcion</div>
-                <div className="grid grid-cols-1">Comentarios</div>
+                <div className="h-133 rounded-md bg-violet-400 m-3">
+
+                </div>
+                <div className="h-50 rounded-md bg-violet-300 p-2 m-3">Descripcion</div>
+                <div className="grid grid-cols-1 gap-2 bg-violet-300 m-3">Comentarios</div>
             </div>
-            <div className=" h-full grid grid-cols-1">Mas videos</div>
-            
+            <div className=" h-full grid grid-cols-1 gap-3  bg-violet-300 m-3 p-2">Mas videos</div>
          </div>
         </>
     );
