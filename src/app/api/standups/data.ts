292d91ca7ec39b9cad1205519cec58f2c40bc9f0
Intellisys <intellisys@Intellisyss-MacBook-Pro-3.local>
2025-09-22 16:24:24 -0400

    Added base to API

---
+++ src/app/api/standups/data.ts
@@ -0,0 +1,13 @@
+type Standup = {
+    id: number;
+    title: string;
+    description: string;
+}
+
+export const STANDUPS = [
+    {
+        id: 1,
+        title: "Daily Standup",
+        description: "Daily team standup meeting",
+    }
+]
---
+++ src/app/api/standups/route.ts
@@ -0,0 +1,5 @@
+import { STANDUPS } from "./data";
+
+export async function GET(request: Request) {
+    return Response.json({ data: STANDUPS });
+}
