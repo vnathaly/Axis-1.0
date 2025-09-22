69a396c0c26f603cb6a4e1a452d7073a52f06a34
Intellisys <intellisys@Intellisyss-MacBook-Pro-3.local>
2025-09-22 15:59:54 -0400

    adjust layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,14 @@ import { Header } from "@/components/header";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
-export default function RootLayout({
+export default async function RootLayout({
+  
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
+    
     <ClerkProvider>
     <html lang="en">
       <body>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
 import { Protect } from '@clerk/nextjs';
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware(  async() => await auth.protect() );
+const isProtectedRoute = createRouteMatcher(['(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+} );
 
 export const config = {
   matcher: [
