b79ed6a56accca22f5c5b09e7eaf426fac19654b
Intellisys <intellisys@Intellisyss-MacBook-Pro-3.local>
2025-09-22 16:12:35 -0400

    remove unused CSS

--- src/app/reprolist/page.tsx
+++ src/app/reprolist/page.tsx
@@ -5,11 +5,11 @@ export default function reproList() {
         <>
          <div className="grid grid-cols-3 p-9"> 
             <div className="w-auto h-screem col-span-2">
-                <div className="h-133 standups m-3 ">
+                <div className="h-133 rounded-md bg-slate-500 m-3 ">
                     {/* Aqui va un video */}
                 </div>
-                <div className="h-50 standups p-2 m-3">Descripcion</div>
-                <div className="grid grid-cols-1 gap-2 standups m-3 p-2">Comentarios</div>
+                <div className="h-50 rounded-md bg-slate-500 p-2 m-3">Descripcion</div>
+                <div className="grid grid-cols-1 gap-2 rounded-md bg-slate-500 m-3 p-2">Comentarios</div>
             </div>
             <div className=" h-full grid grid-cols-1 gap-3 p-2">
                 <div className="mt-1 standups">
@@ -19,14 +19,14 @@ export default function reproList() {
                         </video>
                     </figure>
                 </div>
-                <div className="standups">
+                <div className="rounded-md bg-slate-500">
                     <figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure>
                 </div>
-                <div className="standups">
+                <div className="rounded-md bg-slate-500">
                     <figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
--- src/app/standups/page.tsx
+++ src/app/standups/page.tsx
@@ -2,53 +2,53 @@ export default function Standup() {
     return (
     <> 
         <div className="grid grid-cols-4 p-9 gap-3 ">
-            <div className="bg-gray-600 w-full rounded-md"></div>
+            <div className="bg-slate-500 w-full rounded-md"></div>
             <div className="w-full h-screen col-span-3 grid grid-cols-3 gap-2">
-                <div className="standups">
+                <div className="rounded-md bg-slate-500">
                     <figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure>
                 </div>
-                <div className="standups">
+                <div className="rounded-md bg-slate-500">
                     <figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure>
                 </div>
-                <div className="standups"><figure>
+                <div className="rounded-md bg-slate-500"><figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure></div>
-                <div className="standups"><figure>
+                <div className="rounded-md bg-slate-500"><figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure></div>
-                <div className="standups"><figure>
+                <div className="rounded-md bg-slate-500"><figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure></div>
-                <div className="standups"><figure>
+                <div className="rounded-md bg-slate-500"><figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure></div>
-                <div className="standups"><figure>
+                <div className="rounded-md bg-slate-500"><figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure></div>
-                <div className="standups"><figure>
+                <div className="rounded-md bg-slate-500"><figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
                     </figure></div>
-                <div className="standups"><figure>
+                <div className="rounded-md bg-slate-500"><figure>
                         <video className="w-full" src="/videoprueba.mp4">
                             {/* <source src="/videoprueba.mp4"/> */}
                         </video>
